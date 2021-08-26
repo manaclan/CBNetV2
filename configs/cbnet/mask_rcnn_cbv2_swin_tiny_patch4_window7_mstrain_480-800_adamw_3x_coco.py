39a625f0f02645f6ba6772ed590e62969f710ae8
vinh.ngo <vinh.ngo@eyeq.tech>
2021-08-26 23:02:52 +0700

    update data path

--- configs/cbnet/mask_rcnn_cbv2_swin_tiny_patch4_window7_mstrain_480-800_adamw_3x_coco.py
+++ configs/cbnet/mask_rcnn_cbv2_swin_tiny_patch4_window7_mstrain_480-800_adamw_3x_coco.py
@@ -10,3 +10,23 @@ model = dict(
         type='CBFPN',
     ),
 )
+
+dataset_type = 'COCODataset'
+CLASSES = ('cmnd')
+data = dict(
+    train=dict(
+        img_prefix='/content/coco_cmnd/train2017',
+        classes=classes,
+        ann_file='/content/coco_cmnd/annotations/instances_train2017.json'),
+    val=dict(
+        img_prefix='/content/coco_cmnd/val2017',
+        classes=classes,
+        ann_file='/content/coco_cmnd/annotations/instances_val2017.json'))
+    '''test=dict(
+        img_prefix='balloon/val/',
+        classes=classes,
+        ann_file='balloon/val/annotation_coco.json'))'''
+
+# We can use the pre-trained Mask RCNN model to obtain higher performance
+#load_from = './checkpoints/mask_rcnn_r50_caffe_fpn_mstrain-poly_3x_coco_bbox_mAP-0.408__segm_mAP-0.37_20200504_163245-42aa3d00.pth'
+load_from = '/content/model/mask_rcnn_cbv2_swin_tiny_patch4_window7_mstrain_480-800_adamw_3x_coco.pth' 
